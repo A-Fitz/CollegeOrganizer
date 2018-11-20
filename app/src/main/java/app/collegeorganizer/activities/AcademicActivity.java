db8e96fc9278a6f213da094948c719c2455dbcd5
Ross Baldwin <baldwinros@uwplatt.edu>
2018-11-20 14:50:31 -0600

    TabLayout for AcademicActivity implemented, ListView added to the class schedule fragment

--- app/src/main/java/app/collegeorganizer/activities/AcademicActivity.java
+++ app/src/main/java/app/collegeorganizer/activities/AcademicActivity.java
@@ -1,4 +1,4 @@
-package app.collegeorganizer;
+package app.collegeorganizer.activities;
 
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
@@ -18,6 +18,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.TextView;
+
+import app.collegeorganizer.R;
 
 public class AcademicActivity extends AppCompatActivity {
 
